f640cdcdc3345722ba1821ea8e40af0893b77df4
DDDavidSt <david.stevanak@gmail.com>
2022-09-01 01:08:11 +0200

    setting up django

--- mandalinka/mandalinka/settings.py
+++ mandalinka/mandalinka/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o!mapk53^97^a6xfm4at_+x*^e0h0(rntpd4$6(msgjdc=0aad'
+SECRET_KEY = 'django-insecure-hj454))ma)&o185ju&z^%n_s$2r7exjk+mg)&_8%_xpig)z734'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
