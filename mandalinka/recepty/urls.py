84eb998241362250756fa881d35f3b844accf8dc
Timur Kramár <timurjan.kramar123@gmail.com>
2022-09-01 10:19:02 +0200

    Rozšírenie základného stromu urls

--- mandalinka/recepty/urls.py
+++ mandalinka/recepty/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('novy_recept', views.novy_recept, name='novy_recept')
 ]
 
--- mandalinka/recepty/views.py
+++ mandalinka/recepty/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. Nachádzate sa v aplikácií recepty")
+    return HttpResponse("Táto stránka bude zobrazovať zoznam pridaných receptov")
+
+def novy_recept(request):
+    return HttpResponse("Na tejto stránke sa budú pridávať nové recepty")
