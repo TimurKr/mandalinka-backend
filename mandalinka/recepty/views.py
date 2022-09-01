e61dd8fe6e8d46cc5076034fbcfd3a1065433461
Timur Kramár <timurjan.kramar123@gmail.com>
2022-09-01 17:58:33 +0200

    Periodic commit

--- mandalinka/recepty/views.py
+++ mandalinka/recepty/views.py
@@ -1,8 +1,33 @@
+from xmlrpc.client import boolean
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Krok():
+    
+    thumbnail = None
+    postup_sk = None
+    postup_en = None
+
+    def __init__(self) -> None:
+        pass
+
+
+class Recept():
+    thumbnail = "static"
+    nazov = "Mandalinkový koláč"
+    postup = [Krok() for i in range(6)]
+
+    def __init__(self) -> None:
+        pass
+
+
+recepty = [Recept() for i in range(3)]
+
 # Create your views here.
 def index(request):
-    return render(request, "recepty/recepty_homeview.html")
+    return render(request, "recepty/recepty_homeview.html", {
+        "recepty": recepty
+    })
+
 def novy_recept(request):
-    return HttpResponse("Na tejto stránke sa budú pridávať nové recepty")
+    return render(request, "recepty/novy_recept.html")
