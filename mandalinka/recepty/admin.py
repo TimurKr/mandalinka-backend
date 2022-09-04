9cae4b33059243bd05da3469f72c5610491d1405
Timur Kramár <timurjan.kramar123@gmail.com>
2022-09-04 20:45:05 +0200

    Recipe-Ingredient relationship fixed, new db.sqlite

--- mandalinka/recepty/admin.py
+++ mandalinka/recepty/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 
-from .models import Recipe, Ingredient, Alergen
+from .models import Recipe, Ingredient, Alergen, IngredientInstance
 
 # Site customizations
+class IngredientInstanceInline(admin.TabularInline):
+    model = IngredientInstance
+    extra = 1
+
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "description")
     filter_horizontal = ("ingredients",)
+    inlines = (IngredientInstanceInline,)
 
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "unit")
     filter_horizontal = ("alergens",)
+    inlines = (IngredientInstanceInline,)
+
 
 # class Alergen(admin.ModelAdmin):
 #     list_display = ("id", "title")
@@ -19,3 +26,4 @@ class IngredientAdmin(admin.ModelAdmin):
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Alergen)
+admin.site.register(IngredientInstance)
--- mandalinka/recepty/models.py
+++ mandalinka/recepty/models.py
@@ -35,7 +35,13 @@ class Alergen(models.Model):
 
     def __str__(self):
         return f"{self.code}: {self.title}"
-    
+
+
+class IngredientInstance(models.Model):
+    ingredient = models.ForeignKey("Ingredient", on_delete=models.PROTECT)
+    recipe = models.ForeignKey("Recipe", on_delete=models.CASCADE)
+    amount = models.IntegerField(primary_key=True, verbose_name="Množstvo", help_text="Zadajte množstvo danej potraviny")
+
 
 class Ingredient(models.Model):
     title = models.CharField(max_length=31, unique=True, verbose_name="Názov", help_text="Názov ingrediencie")
@@ -48,7 +54,7 @@ class Ingredient(models.Model):
         ("ml", "mililitre")]
     unit = models.CharField(max_length=3, choices=UNITS_TO_SELECT, verbose_name="Jednotka", help_text="Zvolte jednotku")
 
-    alergens = models.ManyToManyField(Alergen, blank=True, help_text="Zvolte všetky alergény:", verbose_name="Alergény", related_name="ingredients")
+    alergens = models.ManyToManyField(Alergen, blank=True, help_text="Zvolte všetky alergény:", verbose_name="Alergény", related_name="Ingredients")
 
     def __str__(self):
         return f"{self.title}"
@@ -58,7 +64,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=63, unique=True, help_text="Názov receptu")
     description = models.TextField(max_length=127, verbose_name="Opis jedla", help_text="Zadajte stručný opis jedla")
     prep_time = models.IntegerField(verbose_name="Čas prípravy", help_text="Zadajte dĺžku prípravy")
-    ingredients=models.ManyToManyField(Ingredient, blank=False, help_text="Zvolte všetky ingrediencie")
+    ingredients = models.ManyToManyField(Ingredient, through=IngredientInstance, blank=False, help_text="Zvolte všetky ingrediencie", related_name="Recipes")
     thumbnail = models.ImageField(upload_to=f"recepty/static/photos/", help_text="Pridajte thumbnail", default=None)
 
     # steps = models.TextField(max_length=1027, verbose_name=f"Kroky", help_text=f"Zadajte kroky postupu")
