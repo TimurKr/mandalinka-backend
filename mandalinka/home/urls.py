2c07fe525494db8617b9ae8a71656217a8b4d510
DDDavidSt <david.stevanak@gmail.com>
2022-09-05 21:58:35 +0200

    added login form and basic logging-in/out logic

--- mandalinka/home/urls.py
+++ mandalinka/home/urls.py
@@ -1,7 +1,12 @@
 # pages/urls.py
 from django.urls import path
 from .views import HomePageView
+from . import views
+
+app_name = 'home'
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- mandalinka/home/views.py
+++ mandalinka/home/views.py
@@ -1,6 +1,51 @@
 from django.views.generic import TemplateView
+from django import forms
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-
+from django.contrib.auth import authenticate, login, logout
 
 class HomePageView(TemplateView):
     template_name = "home/home.html"
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label="username", widget=forms.TextInput({'class':'form-control'}))
+    password = forms.CharField(label="password", widget=forms.PasswordInput())
+ 
+# Create your views here.
+def index(request):
+    return render(request, "home/home.html")
+
+def login_view(request):
+    
+    if request.method == "POST":
+        print(request.POST)
+        form = LoginForm(request.POST)
+        # Accessing username and password from form data
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Check if username and password are correct, returning User object if so
+        user = authenticate(request, username=username, password=password)
+
+        # If user object is returned, log in and route to index page:
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home:home"))
+        # Otherwise, return login page again with new context
+        else:
+            return render(request, "home/login.html", {
+                "message": "Invalid Credentials",
+                "form": form
+            })
+    return render(request, "home/login.html", {
+        "form": LoginForm()
+    })
+
+def logout_view(request):
+    logout(request)
+    return render(request, "home/login.html", {
+                "message": "Logged Out",
+                "form": LoginForm()
+            })
