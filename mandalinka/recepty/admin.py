6d842ca9ec0031bdbac3caeeaa8a0b65d39b60c8
Timur Kramár <timurjan.kramar123@gmail.com>
2022-09-06 14:00:51 +0200

    Edit models, add RecipeVersion

--- mandalinka/recepty/admin.py
+++ mandalinka/recepty/admin.py
@@ -1,25 +1,29 @@
 from django.contrib import admin
 
-from .models import Recipe, Ingredient, Alergen, IngredientInstance, Step
+from .models import Recipe, Ingredient, Alergen, IngredientInstance, Step, RecipeVersion
 
 # Site customizations
+# Has to go befor RecipeVersionAdmin
 class IngredientInstanceInline(admin.TabularInline):
     model = IngredientInstance
     extra = 1
 
+class RecipeVersionAdmin(admin.ModelAdmin):
+    list_display = ("id", "recipe")
+    filter_horizontal = ("steps", "ingredients")
+    inlines = (IngredientInstanceInline,)
+
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "description")
-    filter_horizontal = ("ingredients","steps")
-    inlines = (IngredientInstanceInline,)
 
 class StepsAdmin(admin.ModelAdmin):
     list_display = ("id", "step")
     filter_horizontal = ("recipes",)
 
+
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "unit")
     filter_horizontal = ("alergens",)
-    inlines = (IngredientInstanceInline,)
 
 
 # class Alergen(admin.ModelAdmin):
@@ -27,9 +31,10 @@ class IngredientAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
+admin.site.register(RecipeVersion, RecipeVersionAdmin)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Step, StepsAdmin)
+admin.site.register(IngredientInstance)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Alergen)
-admin.site.register(IngredientInstance)
-admin.site.register(Step, StepsAdmin)
 
--- mandalinka/recepty/models.py
+++ mandalinka/recepty/models.py
@@ -41,7 +41,7 @@ class Alergen(models.Model):
 
 class IngredientInstance(models.Model):
     ingredient = models.ForeignKey("Ingredient", on_delete=models.PROTECT)
-    recipe = models.ForeignKey("Recipe", on_delete=models.CASCADE)
+    recipe = models.ForeignKey("RecipeVersion", on_delete=models.CASCADE)
     amount = models.IntegerField(primary_key=True, verbose_name="Množstvo", help_text="Zadajte množstvo danej potraviny")
 
 
@@ -67,26 +67,32 @@ class Step(models.Model):
     step = models.TextField(max_length=250, help_text="Krok: ")
     step_img = models.ImageField(upload_to=f"recepty/static/photos/", help_text="Pridajte obrazok ku kroku", default=None)
     step_no = models.IntegerField(verbose_name="Krok cislo:", help_text="Zadaj poradie kroku")
-    recipe = models.ForeignKey("Recipe", on_delete=models.CASCADE, related_name="Recipes", null=True, blank=True)
+    recipe = models.ForeignKey("RecipeVersion", on_delete=models.CASCADE, related_name="Recipes", null=True, blank=True)
     
     class Meta:
         # ensuring each recipe has only one unique step_no
         constraints = [
             models.UniqueConstraint(fields=['step_no', 'recipe'], name='Each recipe has a unique set of steps')
         ]
+
     def __str__(self):
         return f"text: {self.step}"
 
 class Recipe(models.Model):
     title = models.CharField(max_length=63, unique=True, help_text="Názov receptu")
     description = models.TextField(max_length=127, verbose_name="Opis jedla", help_text="Zadajte stručný opis jedla")
-    prep_time = models.IntegerField(verbose_name="Čas prípravy", help_text="Zadajte dĺžku prípravy")
-    ingredients = models.ManyToManyField(Ingredient, through=IngredientInstance, blank=False, help_text="Zvolte všetky ingrediencie", related_name="recipes")
     thumbnail = models.ImageField(upload_to=f"recepty/static/photos/", help_text="Pridajte thumbnail", default=None)
-
-    steps = models.ManyToManyField(Step, blank=False, help_text="Pridajte kroky", related_name="recipes")
 
     # Price
 
     def __str__(self):
         return f"{self.title}"
+
+class RecipeVersion(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe_versions")
+    version = models.IntegerField(verbose_name="Verzia receptu", help_text="Zadajte koľkatá je to verzia receptu")
+    prep_time = models.IntegerField(verbose_name="Čas prípravy", help_text="Zadajte dĺžku prípravy")
+    ingredients = models.ManyToManyField(Ingredient, through=IngredientInstance, blank=False, help_text="Zvolte všetky ingrediencie", related_name="recipes")
+    steps = models.ManyToManyField(Step, blank=False, help_text="Pridajte kroky", related_name="recipes")
+
+
