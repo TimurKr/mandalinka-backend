59cd32ec542f1064bc5f7d119eb1ffff6774e7bf
Timur Kramár <timurjan.kramar123@gmail.com>
2023-02-03 11:47:10 +0100

    Adding node dependencies for reasct/mui

--- mandalinka/management/static/management/react/index.jsx
+++ mandalinka/management/static/management/react/index.jsx
@@ -1,1 +1,7 @@
 // This is the entry point for the Management React app.
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
--- mandalinka/webpack.config.js
+++ mandalinka/webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [new BundleTracker({ filename: "./webpack-stats.json" })],
